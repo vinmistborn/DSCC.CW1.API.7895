7b4d7dbef1777250223725e56a43021bc0ea70b5
student-wad <funit00007895@gmail.com>
2021-11-08 11:03:36 -0800

    deploy: configure AWS Elastic Beanstalk, change startup.cs env.IsProduction()

--- DSCC.CW1.API.7895/Startup.cs
+++ DSCC.CW1.API.7895/Startup.cs
@@ -43,7 +43,7 @@ namespace DSCC.CW1.API._7895
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
